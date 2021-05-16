b49378d58430ac8cc59fe825621dc2e8dd8e9a06
chenyuzou <chenyuzou@qq.com>
2021-05-16 23:12:33 +0800

    添加redis缓存

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/go-redis/redis/v8"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"strconv"
@@ -19,6 +22,7 @@ type PhoneSection struct {
 
 type pnl struct {
 	conn *gorm.DB
+	rdb  *redis.Client
 }
 
 var p *pnl
@@ -30,23 +34,49 @@ func NewLocation() *pnl {
 	if err != nil {
 		panic("failed to connect database")
 	}
+	p.rdb = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
 	return p
 }
+
+var ctx = context.Background()
 
 func (p *pnl) Locate(phoneNum string) (ret PhoneSection, err error) {
 	if len(phoneNum) != 11 || phoneNum[0] != 0x31 {
 		return ret, errors.New("invalid phone number")
 	}
 	sectionStr := phoneNum[0:7]
-	section, err := strconv.Atoi(sectionStr)
-	if err != nil {
-		return
+	MacStr := phoneNum[0:3]
+
+	decoded, err := p.rdb.HGet(ctx, "PhoneSection:MAC-"+MacStr, sectionStr).Result()
+
+	if err == nil { // 命中缓存
+		if err = json.Unmarshal([]byte(decoded), &ret); err != nil {
+			return ret, err
+		}
+		fmt.Println("from cache")
+		return ret, nil
+	} else { // 未命中缓存
+		_ = err
+		section, err := strconv.Atoi(sectionStr)
+		if err != nil {
+			return ret, err
+		}
+		result := p.conn.Take(&ret, section)
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return ret, errors.New("not found")
+		}
+		cacheStr, err := json.MarshalIndent(ret, "", "\t")
+		fmt.Println(string(cacheStr))
+		if err2 := p.rdb.HSet(ctx, "PhoneSection:MAC-"+MacStr, sectionStr, string(cacheStr)).Err(); err2 != nil {
+			return ret, err2
+		}
 	}
-	result := p.conn.Take(&ret, section)
-	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-		return ret, errors.New("not found")
-	}
-	return
+	return ret, nil
 }
 
 func main() {
