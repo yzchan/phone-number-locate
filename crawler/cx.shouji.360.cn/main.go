3428f8f2309d6559d7223aabad6432b33d120f11
chenyuzou <chenyuzou@qq.com>
2021-05-25 21:44:54 +0800

    完善360爬取器

--- crawler/cx.shouji.360.cn/main.go
+++ crawler/cx.shouji.360.cn/main.go
@@ -1,36 +1,59 @@
 package main
 
 import (
+	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
 	"sync"
 	"time"
 )
 
 var pool chan int
 
+type RespResult struct {
+	Code int `json:"code"`
+	Data struct {
+		Province string `json:"province"`
+		City     string `json:"city"`
+		Sp       string `json:"sp"`
+	} `json:"data"`
+}
+
 var wg sync.WaitGroup
+var rw sync.RWMutex
 
 func main() {
-	pool = make(chan int, 2000)
+	f, err := os.OpenFile("data.csv", os.O_RDWR, 0666)
+	if err != nil {
+		panic(err)
+	}
+	w := csv.NewWriter(f)
+	var header = []string{"section", "province", "city", "sp"}
+	w.Write(header)
+	pool = make(chan int, 500)
 	wg.Add(1)
 	bT := time.Now()
 	go producer()
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 200; i++ {
 		wg.Add(1)
-		go consumer()
+		go consumer(w)
 	}
-	//time.Sleep(time.Second * 10)
 	wg.Wait()
+	w.Flush()
+
 	eT := time.Since(bT)
 	fmt.Println("Run time: ", eT)
 }
 
-func consumer() {
+func consumer(w *csv.Writer) {
 	var result []byte
 	var err error
 	var resp *http.Response
+	var r RespResult
 	for {
 		sec, ok := <-pool
 		if !ok {
@@ -41,7 +64,11 @@ func consumer() {
 			continue
 		}
 		result, err = ioutil.ReadAll(resp.Body)
+		err = json.Unmarshal(result, &r)
 		fmt.Println("consumer:", string(result))
+		rw.Lock()
+		w.Write([]string{strconv.Itoa(sec), r.Data.Province, r.Data.City, r.Data.Sp})
+		rw.Unlock()
 	}
 	wg.Done()
 }
@@ -49,9 +76,9 @@ func consumer() {
 func producer() {
 
 	// 要采集的号段
-	section1 := []int{134, 135, 136, 137, 138, 139, 150, 151, 152, 157, 158, 159, 188}
-	section2 := []int{130, 131, 132, 155, 156, 186}
-	section3 := []int{133, 153, 189}
+	section1 := []int{134}
+	section2 := []int{130}
+	section3 := []int{133}
 
 	var sections []int
 	sections = append(sections, section1...)
@@ -59,11 +86,10 @@ func producer() {
 	sections = append(sections, section3...)
 	fmt.Println(len(sections))
 
-	var url string
 	for _, section := range sections {
-		for i := 0; i < 1000; i++ {
-			url = fmt.Sprintf("producer: http://cx.shouji.360.cn/phonearea.php?number=%d", section*10000+i)
-			fmt.Println(url)
+		for i := 0; i < 10000; i++ {
+			//url = fmt.Sprintf("producer: http://cx.shouji.360.cn/phonearea.php?number=%d", section*10000+i)
+			//fmt.Println(url)
 			pool <- section*10000 + i
 		}
 	}
