19b5bc0938bbce2ddf52ca3581cfa4ed65c8d135
chenyuzou <chenyuzou@qq.com>
2021-06-04 17:30:14 +0800

    regex parser

--- query/buybook.com.cn/fetch.go
+++ query/buybook.com.cn/fetch.go
@@ -5,23 +5,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"regexp"
-	"strconv"
-	"strings"
 )
 
 const (
 	UrlFmt = "http://www.buybook.com.cn/lb/%s/all/"
-	ReFmt  = `<tr>
-  <td><a href="/sj/{#sec7}/">({#sec7})</a></td>
-  <td>([\p{Han}]*)</td>
-  <td>([\p{Han}]*)</td>
-  <td>(\d*)</td>
-  <td>(\d*)</td>
-  <td>([\p{Han}]*)</td>
-  <td>(.*)
-</td>
-</tr>`
 )
 
 type PhoneLoc struct {
@@ -34,9 +21,10 @@ type PhoneLoc struct {
 	SimCard  string `json:"sim_card"`
 }
 
-// Parser 解析器接口，用于解析接口返回的jsonp格式的数据
+// Parser 解析器接口，用于解析接口返回的html数据
 type Parser interface {
-	Parse(body []byte) PhoneLoc
+	Parse(body []byte, keyword string) PhoneLoc
+	ParseAll(body []byte) []PhoneLoc
 }
 
 var Instance *Queryer
@@ -46,40 +34,48 @@ type Queryer struct {
 }
 
 func NewQueryer() *Queryer {
-	return &Queryer{}
+	return &Queryer{
+		Parser: NewRegexpParser(),
+	}
 }
 
 func init() {
 	Instance = NewQueryer()
 }
 
-func (q *Queryer) Fetch(phone string) (p PhoneLoc, err error) {
-	if len(phone) != 7 && len(phone) != 11 {
-		err = errors.New("invalid phone number")
+func (q *Queryer) Request(url string) (data []byte, err error) {
+	var resp *http.Response
+	if resp, err = http.Get(url); err != nil {
 		return
 	}
-
-	sec := phone[0:7]
-
-	resp, err := http.Get(fmt.Sprintf(UrlFmt, sec[0:4]))
-	if err != nil {
-		return
-	}
-
 	defer resp.Body.Close()
-
-	html, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
+	if data, err = ioutil.ReadAll(resp.Body); err != nil {
 		return
 	}
+	return
+}
 
-	re := regexp.MustCompile(strings.Replace(ReFmt, "{#sec7}", sec, -1))
-	matched := re.FindAllStringSubmatch(string(html), -1)
-
-	if len(matched) > 0 {
-		p.Section, _ = strconv.Atoi(matched[0][1])
-		p.Province, p.City, p.AreaCode, p.Postcode, p.Sp, p.SimCard = matched[0][2], matched[0][3], matched[0][4], matched[0][5], matched[0][6], matched[0][7]
+func (q *Queryer) Fetch(phone string) (p PhoneLoc, err error) {
+	if len(phone) < 7 {
+		return p, errors.New("invalid phone number")
 	}
 
+	var html []byte
+	if html, err = q.Request(fmt.Sprintf(UrlFmt, phone[0:4])); err != nil {
+		return
+	}
+	p = q.Parser.Parse(html, phone[0:7])
 	return
+}
+
+func (q *Queryer) FetchSec(phone string) (plist []PhoneLoc, err error) {
+	if len(phone) < 7 {
+		return plist, errors.New("invalid phone section")
+	}
+	var html []byte
+	if html, err = q.Request(fmt.Sprintf(UrlFmt, phone[0:4])); err != nil {
+		return
+	}
+	plist = q.Parser.ParseAll(html)
+	return plist, nil
 }
--- query/buybook.com.cn/regexp_parser.go
+++ query/buybook.com.cn/regexp_parser.go
@@ -1,1 +1,62 @@
 package buybook
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+const ReFmt = `<tr>
+  <td><a href="/sj/\d{7}/">(\d{7})</a></td>
+  <td>([\p{Han}]*)</td>
+  <td>([\p{Han}]*)</td>
+  <td>(\d*)</td>
+  <td>(\d*)</td>
+  <td>([\p{Han}]*)</td>
+  <td>(.*)
+</td>
+</tr>`
+
+type RegexpParser struct {
+	re *regexp.Regexp
+}
+
+func NewRegexpParser() *RegexpParser {
+	return &RegexpParser{
+		re: regexp.MustCompile(ReFmt),
+	}
+}
+
+func (r RegexpParser) Parse(body []byte, keyword string) (p PhoneLoc) {
+	text := string(body)
+
+	r.re = regexp.MustCompile(strings.Replace(ReFmt, "\\d{7}", keyword, -1))
+	matched := r.re.FindAllStringSubmatch(text, 1)
+
+	if len(matched) == 0 {
+		return
+	}
+	m := matched[0]
+	p.Section, _ = strconv.Atoi(m[1])
+	p.Province, p.City, p.AreaCode, p.Postcode, p.Sp, p.SimCard = m[2], m[3], m[4], m[5], m[6], m[7]
+	return
+}
+
+func (r RegexpParser) ParseAll(body []byte) (plist []PhoneLoc) {
+	text := string(body)
+
+	matched := r.re.FindAllStringSubmatch(text, -1)
+
+	if len(matched) == 0 {
+		return
+	}
+	var p PhoneLoc
+	for _, m := range matched {
+		p.Section, _ = strconv.Atoi(m[1])
+		p.Province, p.City, p.AreaCode, p.Postcode, p.Sp, p.SimCard = m[2], m[3], m[4], m[5], m[6], m[7]
+		plist = append(plist, p)
+	}
+	fmt.Println(plist)
+	return
+}
